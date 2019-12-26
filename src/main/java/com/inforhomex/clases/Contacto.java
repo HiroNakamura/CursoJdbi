534ab7f14d7cc2ec6eba2285e4815be561843275
ARIEL <carraro.ariel@gmail.com>
2019-12-25 18:36:43 -0600

    Se actualizó Contacto.java
    Se agregaron variables de instancia (atributos) a la clase Contacto.java

--- src/main/java/com/inforhomex/clases/Contacto.java
+++ src/main/java/com/inforhomex/clases/Contacto.java
@@ -1,5 +1,42 @@
 package com.inforhomex.clases;
 
 public class Contacto{
+   private int id;
+   private String nombre;
+   private String telefono;
+   
    public Conctacto(){}
+   
+   public Contacto(int id, String nombre, String telefono){
+      super();
+      this.id = id;
+      this.nombre = nombre;
+      this.telefono = telefono;
+   }
+   
+   public int getId(){
+     return id;
+   }
+   public void setId(int id){
+     this.id = id;
+   }
+   
+   public String getNombre(){
+     return nombre;
+   }
+   public void setNombre(String nombre){
+     this.nombre = nombre;
+   }
+   
+   public String getTelefono(){
+     return telefono;
+   }
+   public void setTelefono(String telefono){
+     this.telefono = telefono;
+   }
+   
+   @Override
+   public String toString(){
+     return "Contacto{id: "+id+",nombre: "+nombre+", telefono: "+telefono+"}";
+   }
 }
