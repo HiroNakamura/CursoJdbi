ac0575b57e955676f11f3d769f94362c0b16b360
ARIEL <carraro.ariel@gmail.com>
2019-12-28 10:05:54 -0600

    ContactoDao.java editado
    Se ha agregado un parámetro en el método insertPositional

--- src/main/java/com/inforhomex/interfaces/ContactoDao.java
+++ src/main/java/com/inforhomex/interfaces/ContactoDao.java
@@ -13,7 +13,7 @@ public interface ContactoDao{
    void createTable();
 
    @SqlUpdate("INSERT INTO contactos(id, nombre, telefono) VALUES (?, ?,?)")
-   void insertPositional(int id, String nombre);
+   void insertPositional(int id, String nombre. String telefono);
 
    @SqlUpdate("INSERT INTO contactos(id, nombre,telefono) VALUES (:id, :nombre,:telefono)")
    void insertNamed(@Bind("id") int id, @Bind("nombre") String nombre, @Bind("telefono") String telefono);
