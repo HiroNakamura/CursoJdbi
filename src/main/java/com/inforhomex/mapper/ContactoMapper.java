4b1d3fc405e43f234b9bc15563b8b19df9a8c123
ARIEL <carraro.ariel@gmail.com>
2019-12-27 21:50:37 -0600

    Se ha editado ContactoMapper.java
    Se agregaron variables de instancia y métodos de acceso.

--- src/main/java/com/inforhomex/mapper/ContactoMapper.java
+++ src/main/java/com/inforhomex/mapper/ContactoMapper.java
@@ -1,5 +1,42 @@
 package com.inforhomex.mapper;
 
 public class ContactoMapper{
-
+  private int id;
+   private String nombre;
+   private String telefono;
+   
+   public ConctactoMapper(){}
+   
+   public ContactoMapper(int id, String nombre, String telefono){
+      super();
+      this.id = id;
+      this.nombre = nombre;
+      this.telefono = telefono;
+   }
+   
+   public int getId(){
+     return id;
+   }
+   public void setId(int id){
+     this.id = id;
+   }
+   
+   public String getNombre(){
+     return nombre;
+   }
+   public void setNombre(String nombre){
+     this.nombre = nombre;
+   }
+   
+   public String getTelefono(){
+     return telefono;
+   }
+   public void setTelefono(String telefono){
+     this.telefono = telefono;
+   }
+   
+   @Override
+   public String toString(){
+     return "ContactoMapper{id: "+id+",nombre: "+nombre+", telefono: "+telefono+"}";
+   }
 }
