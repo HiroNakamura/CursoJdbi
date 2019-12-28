9c764595fd9beb9472bc8d62ff097215595b9deb
ARIEL <carraro.ariel@gmail.com>
2019-12-27 21:01:17 -0600

    Se ha actualizado IServiceCrud.java
    Se han definido las operaciones básicas CRUD: CREATE, READ,  UPDATE y DELETE.
    
    Es una interface tipo 'diamante'.

--- src/main/java/com/inforhomex/interfaces/IServiceCrud.java
+++ src/main/java/com/inforhomex/interfaces/IServiceCrud.java
@@ -1,5 +1,16 @@
 package com.inforhomex.interfaces;
 
-public interface IServiceCrud{
+import java.util.Set;
 
+/**
+*@author Fer Carraro
+*@date 27/12/2019
+*/
+
+public interface IServiceCrud<T,ID>{
+  public abstract Set<T> findAll();
+  public abstract T findById(ID id);
+  public abstract T save(T object);
+  public abstract void delete(T object);
+  public abstract void deleteById(ID id);
 }
