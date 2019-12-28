39c18a6c064a9ac35b0700d386c3ecab4f7e2b42
Fer_Carraro <carraro.fernando@gmail.com>
2019-12-28 10:02:32 -0600

    Se ha editado ContactoDao.java; Se edito pom.xml y se agrego dependencis necesarias

--- src/main/java/com/inforhomex/interfaces/ContactoDao.java
+++ src/main/java/com/inforhomex/interfaces/ContactoDao.java
@@ -2,18 +2,23 @@ package com.inforhomex.interfaces;
 
 import com.inforhomex.clases.Contacto;
 import java.util.List;
+import org.skife.jdbi.v2.sqlobject.Bind;
+import org.skife.jdbi.v2.sqlobject.SqlQuery;
+import org.skife.jdbi.v2.sqlobject.SqlUpdate;
+import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapper;
+
 
 public interface ContactoDao{
-   @SqlUpdate("CREATE TABLE contactos (id INTEGER PRIMARY KEY, nombre VARCHAR)")
+   @SqlUpdate("CREATE TABLE contactos (id INTEGER PRIMARY KEY, nombre VARCHAR, telefono VARCHAR)")
    void createTable();
 
-   @SqlUpdate("INSERT INTO contactos(id, nombre) VALUES (?, ?)")
+   @SqlUpdate("INSERT INTO contactos(id, nombre, telefono) VALUES (?, ?,?)")
    void insertPositional(int id, String nombre);
 
-   @SqlUpdate("INSERT INTO contactos(id, nombre) VALUES (:id, :nombre)")
-   void insertNamed(@Bind("id") int id, @Bind("nombre") String nombre);
+   @SqlUpdate("INSERT INTO contactos(id, nombre,telefono) VALUES (:id, :nombre,:telefono)")
+   void insertNamed(@Bind("id") int id, @Bind("nombre") String nombre, @Bind("telefono") String telefono);
 
-   @SqlUpdate("INSERT INTO contactos(id, nombre) VALUES (:id, :nombre)")
+   @SqlUpdate("INSERT INTO contactos(id, nombre,telefono) VALUES (:id, :nombre,:telefono)")
    void insertBean(@BindBean Contacto contacto);
 
    @SqlQuery("SELECT * FROM contactos ORDER BY nombre")
