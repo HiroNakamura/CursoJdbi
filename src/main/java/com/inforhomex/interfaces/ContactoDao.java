dfc013782c4224385902499a4e995e561e2741cb
Fer_Carraro <carraro.fernando@gmail.com>
2020-03-22 19:52:43 -0600

    Se edito ContactoDao.java

--- src/main/java/com/inforhomex/interfaces/ContactoDao.java
+++ src/main/java/com/inforhomex/interfaces/ContactoDao.java
@@ -4,13 +4,22 @@ import com.inforhomex.clases.Contacto;
 import java.util.List;
 
 import org.skife.jdbi.v2.sqlobject.SqlUpdate; 
+import org.skife.jdbi.v2.sqlobject.Bind;
+import org.skife.jdbi.v2.sqlobject.SqlQuery;
+//import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapper;
+import org.skife.jdbi.v2.DBI;
+import org.skife.jdbi.v2.Handle;
+import org.skife.jdbi.v2.tweak.*;
+import org.skife.jdbi.v2.*;
+
+
 
 public interface ContactoDao{
    
    @SqlUpdate("CREATE TABLE contactos (id INTEGER PRIMARY KEY, nombre VARCHAR(33), telefono VARCHAR(12))")
    void createTable();
 
-   /*
+   
    @SqlUpdate("INSERT INTO contactos(id, nombre, telefono) VALUES (?, ?,?)")
    void insertPositional(int id, String nombre, String telefono);
 
@@ -18,10 +27,10 @@ public interface ContactoDao{
    void insertNamed(@Bind("id") int id, @Bind("nombre") String nombre, @Bind("telefono") String telefono);
 
    @SqlUpdate("INSERT INTO contactos(id, nombre,telefono) VALUES (:id, :nombre,:telefono)")
-   void insertBean(@BindBean Contacto contacto);
+   //void insertBean(@BindBean Contacto contacto);
 
    @SqlQuery("SELECT * FROM contactos ORDER BY nombre")
-   @RegisterBeanMapper(Contacto.class)
+   //@RegisterBeanMapper(Contacto.class)
    List<Contacto> listContactos();
-   */
+   
 }
