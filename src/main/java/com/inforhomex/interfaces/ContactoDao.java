8e7bb495df022ad2c7b013b52422d3d8bc58b5bd
Fer_Carraro <carraro.fernando@gmail.com>
2020-03-23 17:11:31 -0600

    Cambios es ContactoDao y contactoDaoImpl.java

--- src/main/java/com/inforhomex/interfaces/ContactoDao.java
+++ src/main/java/com/inforhomex/interfaces/ContactoDao.java
@@ -13,18 +13,15 @@ import org.skife.jdbi.v2.tweak.*;
 import org.skife.jdbi.v2.*;
 import org.skife.jdbi.v2.sqlobject.BindBean;
 
-
-
 public interface ContactoDao{
+   @SqlUpdate("create table contacto (id int primary key, name varchar(100))")
+   void createSomethingTable();
    
-   @SqlUpdate("create table something (id int primary key, name varchar(100))")
-  void createSomethingTable();
-
-  @SqlUpdate("insert into something (id, name) values (:id, :name)")
-  void insert(@Bind("id") int id, @Bind("name") String name);
-
-  @SqlQuery("select name from something where id = :id")
-  String findNameById(@Bind("id") int id);
-  void close();
+   @SqlUpdate("insert into contacto (id, name) values (:id, :name)")
+   void insert(@Bind("id") int id, @Bind("name") String name);
+   
+   @SqlQuery("select name from contacto where id = :id")
+   String findNameById(@Bind("id") int id);
+   void close();
    
 }
---
+++ src/main/java/com/inforhomex/interfaces/ContactoDaoImpl.java
@@ -0,0 +1,27 @@
+package com.inforhomex.interfaces;
+
+import com.inforhomex.clases.Contacto;
+import java.util.Set;
+
+public class ContactoDaoImpl implements IServiceCrud<Contacto,Integer>{
+    
+    public Set<Contacto> findAll(){
+        return null;
+    }
+    
+    public  Contacto findById(Integer id){
+      return null;
+    }
+    
+    public  Contacto save(Contacto object){
+        return null;
+    }
+    
+    public  void delete(Contacto object){
+
+    }
+    
+    public  void deleteById(Integer id){
+        
+    }
+}
